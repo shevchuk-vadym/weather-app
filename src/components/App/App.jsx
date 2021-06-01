4313761dc77a43a39843f7ad8a88be462a3e9e05
Shevchuk-Vadym <lorelliand@gmail.com>
2021-06-01 22:11:28 +0300

    with css

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import './app.css';
 const appId = process.env;
 
 export class App extends React.Component {
@@ -14,15 +14,45 @@ export class App extends React.Component {
     console.log(this.state.data);
   }
   render() {
-    if(!this.state.data || !this.state.data.main) {
-      return <h2>LOADING...</h2>
+    if (!this.state.data || !this.state.data.main) {
+      return <h2>LOADING...</h2>;
     }
     return (
       <div className='main'>
-        <div className='temperature'>{this.state.data.main.temp}</div>
-        <div className='temperature'>{this.state.data.hello.test}</div>
-        <div className='weather'></div>
-        <div className='location'>Город {this.state.data.temp}</div>
+        <div className='temp'>
+          <div className='temp__main'>
+            <p>темепатура:</p>
+            <p>{this.state.data.main.temp} C</p>
+          </div>
+          <div className='temp__feel'>
+            <p>ощущается как:</p>
+            <p>{this.state.data.main.feels_like} C</p>
+          </div>
+          <div className='temp__min'>
+            <p>мин. темепатура:</p>
+            <p>{this.state.data.main.temp_min} C</p>
+          </div>
+          <div className='temp__max'>
+            <p>макс. температура:</p>
+            <p>{this.state.data.main.temp_max} C</p>
+          </div>
+        </div>
+        <div className='weather'>
+          <div className='wind'>
+            <div className='wind__main'>
+              <p>скорость ветра:</p>
+              <p>{this.state.data.wind.speed} м/c</p>
+            </div>
+            <div className='wind__gust'>
+              <p>порывы ветра:</p>
+              <p>{this.state.data.wind.gust} м/c</p>
+            </div>
+          </div>
+        </div>
+        <div className='location'>
+          <h2>Город:</h2>
+          <h2>{this.state.data.name}</h2>
+        </div>
       </div>
     );
   }
