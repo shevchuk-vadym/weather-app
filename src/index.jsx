c89afd9c528405a8e11b3f210f883c20ff535ed6
Shevchuk-Vadym <lorelliand@gmail.com>
2021-08-16 18:29:52 +0300

    123

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { App } from './components/App';
-
+console.log('HELLO');
 ReactDOM.render(<App />, document.querySelector('#root'));
